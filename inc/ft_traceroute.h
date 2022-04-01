3fa07dbd988d86b7d569e5928ee38e5d5821975b
lucasmln <lucas.moulin78114@gmail.com>
2022-04-01 00:35:20 +0000

    add proto, clean define

--- inc/ft_traceroute.h
+++ inc/ft_traceroute.h
@@ -18,37 +18,36 @@
 # define UDP_PORT 33434
 # define MAX_ROUND 3
 # define MAX_HOP 30
+# define USEC 1000000
+# define PACKET_LEN_MAX 70000
+# define MTU 1500
 
 # define SUCCESS_CODE 0
 # define ERROR_CODE 1
-# define TIMEOUT_CODE 2
-# define NOT_GOOD_IP 3
-# define INVALID_PACKET 4
 
 # define BSWAP16(x)			((__uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
 
 # define FLAGS_I 0b00000001 // Send ICMP packets
 # define FLAGS_W 0b00000010 // Set wait time for response
 # define FLAGS_Z 0b00000100 // Set interval time between probes
+# define FLAGS_F 0b00001000 // Set the value of the first ttl
+# define FLAGS_M 0b00010000 // Set the max ttl value
+# define FLAGS_N 0b00100000 // Do not map ip addresses with host names
+# define FLAGS_P 0b01000000 // For UDP set the destination port. It will be incremented. For ICMP set the sequence value
+# define FLAGS_Q 0b10000000 // Set the number of probe packet per hop. The default is 3
 
-# define Green "\033[0;32m"
-# define Red "\033[0;31m"
-# define White "\033[0;37m"
-# define Blue "\033[0;34m"
-# define Yellow "\033[0;33m"
 
 typedef struct		udp_packet_s
 {
 	struct ip		ip_header;
 	struct udphdr	udp_header;
-	char			data[60 - sizeof(struct udphdr) - sizeof(struct ip)];
+	uint8_t			data[PACKET_LEN_MAX + 1];//char			data[60 - sizeof(struct udphdr) - sizeof(struct ip)];
 }					udp_packet_t;
 
 typedef struct		icmp_packet_s
 {
-	//struct ip		ip_header;
-	struct icmp		icmp_header;
-	char			data[60 - sizeof(struct icmp) - sizeof(struct ip)];
+	struct icmphdr	icmp_header;
+	uint8_t			data[PACKET_LEN_MAX + 1];
 }					icmp_packet_t;
 
 
@@ -56,15 +55,19 @@ typedef struct		ft_traceroute_s
 {
 	int					raw_sock;
 	int					icmp_sock;
-	int					pid;
 	struct sockaddr_in	addr;
 	struct sockaddr_in	rcv;
 	void				*pck;
 	char				*last_ip;
 	char				*addr_ip;
+	int					pid;
 	int					ttl;
 	int					seq;
+	int					nb_probes;
+	int					ttl_max;
+	int					packet_len;
 	int					flags;
+	unsigned int		default_port;
 	double				wait_recv;
 	double				wait_probes;
 }					ft_traceroute_t;
@@ -72,39 +75,55 @@ typedef struct		ft_traceroute_s
 ft_traceroute_t		g_data;
 
 
-void	save_time(struct timeval *time);
+/*
+ ** main.c
+*/
+void			free_traceroute();
+void			loop_traceroute();
+void			init();
 
 /*
  ** socket.c
 */
-void	create_socket();
-void	error_socket(const char *error);
-void	dns_lookup(const char *dest);
+void			create_socket();
+void			error_socket(const char *error);
+void			dns_lookup(const char *dest);
+char			*reverse_dns_lookup(struct sockaddr_in add);
 
 /*
  ** packet.c
 */
-void	format_udp_header(int ttl, udp_packet_t *pck);
-int		send_packet(void *pck);
-int		rcv_packet(struct timeval *time);
-int		check_packet(char *buf, struct sockaddr_in *from, struct timeval *time);
-unsigned short	checksum(void *address, int len);
+void			format_udp_header(int ttl, udp_packet_t *pck);
+int				send_packet(void *pck);
+int				rcv_packet(struct timeval *time);
+int				check_packet(char *buf, struct sockaddr_in *from, struct timeval *time);
+void			format_icmp_header(int ttl, icmp_packet_t *icmp);
 
 
 /*
  ** parse.c
 */
-void	parse(int ac, char **av);
-void	check_option(int ac, char **av, int i);
+void			parse(int ac, char **av, int *addr_pos);
+void			get_options(int ac, char **av, int *i);
+void			check_flags();
+int				get_packet_len(char **av, int ac, int *i);
+char			get_flag(char *flag);
+void			set_flag_dvalue(double *dst, int pos, char **av, int ac);
+void			set_flag_ivalue(int *dst, int pos, char **av, int ac);
 
 /*
  ** utils.c
 */
-void	free_traceroute();
-void	save_time(struct timeval *time);
-void	print_packet(int round, int ttl, struct timeval time, int code);
-char	*reverse_dns_lookup(struct sockaddr_in add);
-void	print_usage();
+void			print_traceroute_hdr(char *dst, char *dns_dst, int hops, long unsigned int pck_size);
+void			print_packet(int round, int ttl, struct timeval time, int code);
+void			print_usage();
+unsigned short	checksum(void *address, int len);
+
+/*
+ ** time.c
+*/
+void			wait_time(double time_sec);
+void			save_time(struct timeval *time);
 struct timeval	timeval_sub(struct timeval *a, struct timeval *b);
 
 #endif
